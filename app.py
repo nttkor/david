1e10a7ac9ece99fc302863e0d1815fcb5753a982
nttkor <mpeg4@ymail.com>
2025-08-05 18:46:56 +0900

    app without rout

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-# add-image asa
-def main():
-    print('hello')
-    return
-if __name__ == '__main__':
-    main()   
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<a href="/menu">메뉴로 이동</a>'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
